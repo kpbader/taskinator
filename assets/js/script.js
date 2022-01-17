fc77f3830a8a619524529b1ecba36409e87dce2d
Kyle Patrick Bader <kbader91@gmail.com>
2022-01-17 12:01:45 -0800

    add data persistence

--- assets/js/script.js
+++ assets/js/script.js
@@ -231,6 +231,25 @@ var saveTasks = function() {
     localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
+// LOAD TASKS FUNCTION 
+var loadTasks = function() {
+    // reassign tasks
+   var savedTasks = localStorage.getItem("tasks");
+
+    if (!savedTasks) {
+        return false;
+    }
+
+    savedTasks = JSON.parse(savedTasks);
+
+    // loop through savedTasks array 
+    for (var i = 0; i < savedTasks.length; i++) {
+        // pass each task object into the `createTaskEl()` function 
+        createTaskEl(savedTasks[i]);
+    }
+};
+
 pageContentEl.addEventListener("change", taskStatusChangeHandler);
 pageContentEl.addEventListener("click", taskButtonHandler);
 
+loadTasks();
